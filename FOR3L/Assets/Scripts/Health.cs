4b3680be0cdc9a060ee4b70b7c2f703a610205e5
Stimmikex <styrmir97@gmail.com>
2017-11-08 15:20:13 +0000

    Added objects that can kill the player

---
+++ FOR3L/Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    private level_manager the_level_manager;
+
+	// Use this for initialization
+	void Start () {
+        the_level_manager = FindObjectOfType<level_manager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            the_level_manager.Respawn();
+        }
+    }
+}
--- FOR3L/Assets/Scripts/level_manager.cs
+++ FOR3L/Assets/Scripts/level_manager.cs
@@ -8,6 +8,8 @@ public class level_manager : MonoBehaviour {
 
     public player_controller the_player;
 
+    public GameObject death_blood;
+
 	// Use this for initialization
 	void Start () {
         the_player = FindObjectOfType<player_controller>();
@@ -20,8 +22,19 @@ public class level_manager : MonoBehaviour {
 
     public void Respawn()
     {
+        StartCoroutine("RespawnCo");
+    }
+
+    public IEnumerator RespawnCo()
+    {
         /*This will deactivate the player*/
         the_player.gameObject.SetActive(false);
+
+        /*This will create a object. it will crate the blood animation for the death of the player*/
+        Instantiate(death_blood, the_player.transform.position, the_player.transform.rotation);
+
+        yield return new WaitForSeconds(respawn_timer);
+
         /*This will change the posistion where the player died*/
         the_player.transform.position = the_player.respawn_pos;
         /*This will active the player*/
