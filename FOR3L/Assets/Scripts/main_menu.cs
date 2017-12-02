9eeebfd67b0432c5b4ac79267ff60a7407d18f38
Stimmikex <styrmir97@gmail.com>
2017-12-02 18:41:47 +0000

    Added main menu

---
+++ FOR3L/Assets/Scripts/main_menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main_menu : MonoBehaviour {
+
+    public string first_level;
+    public string level_select;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    /*if the new game button is pressed*/
+    public void new_game()
+    {
+        SceneManager.LoadScene(first_level);
+    }
+    /*if the contine game button is pressed*/
+    public void continue_game()
+    {
+        SceneManager.LoadScene(level_select);
+    }
+    /*if the quit game button is pressed*/
+    public void quit_game()
+    {
+        Application.Quit();
+    }
+}
